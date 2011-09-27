5c20658998d01eb55ce83dfaa94376fc2f4ab428
mrudhukar <mrudhu@gmail.com>
2011-09-27 23:54:41 +0530

    Trying to fix pagination styles

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,3 +22,83 @@ $(document).ready(function(){
     $('ul.columnize').makeacolumnlists({cols:3,colWidth:100, equalHeight:true, startN:1});
     $("table.sortenabled").tablesorter({ sortList: [[1,0]] });
 })
+
+var CommonItem = {
+  elements: [],
+
+  tType: function(){
+    return $('#common_item_transaction_type').val();
+  },
+
+  typeChange: function(url){
+    var url_with_params = url + '?type=' + this.tType();
+    $('loading_image').show();
+    new Ajax.Request(url_with_params, {asynchronous:true, evalScripts:true, method:'get'});
+    return false;
+  },
+
+  observeAll: function(){
+//    for(i =0 ; i < this.elements.length; i++){
+//    Event.observe(this.elements[i], 'keyup',   function(){CommonItem.update_cost();}, false);
+//    Event.observe(this.elements[i], 'keydown', function(){CommonItem.update_cost();}, false);
+//    Event.observe(this.elements[i], 'click',   function(){CommonItem.update_cost();}, false);
+//    Event.observe(this.elements[i], 'blur', function(){CommonItem.update_cost();}, false);
+//    }
+  },
+
+  update_cost: function(){
+    $('common_item_cost').disabled = false;
+    $('common_item_cost').value = 0;
+    var sum = 0;
+    for(i =0 ; i < this.elements.length; i++){
+      sum += parseInt(this.elements[i].value || 0);
+    }
+    $('common_item_cost').value = sum;
+
+    if(this.tType() != "0"){
+      $('common_item_cost').disabled = true;
+    }
+  },
+
+  update_final_cost: function (){
+    this.update_cost();
+
+    if(isNaN($('common_item_cost').value)){
+      alert("Give valid values for the items");
+      return false;
+    }
+    $('common_item_cost').disabled = false;
+    return true;
+  },
+
+  beforeSubmit: function(){
+    if(this.tType() == "0"){
+      var theForm = $('#new_common_item input[type=checkbox]');
+      var z = 0;
+      var count = 0;
+
+      for (z = 0; z < theForm.length; z++) {
+        if (theForm[z].checked) {
+          count += 1;
+        }
+      }
+      if(count == 0){
+        alert("Please select atleast one user!");
+        return false;
+      }
+    }else{
+      $('common_item_cost').disabled = false;
+      return this.update_final_cost();
+    }
+  },
+
+  selectAll: function(state){
+    var theForm = $('#new_common_item input[type=checkbox]');
+    var z = 0;
+    for (z = 0; z < theForm.length; z++) {
+      theForm[z].checked = state;
+    }
+    $('#select').toggle();
+    $('#unselect').toggle();
+  }
+}
--- app/helpers/common_items_helper.rb
+++ app/helpers/common_items_helper.rb
@@ -19,7 +19,9 @@ module CommonItemsHelper
 
   def show_delete_link(common_item)
     if current_user == common_item.user
-      link_to "Delete", group_common_item_path(common_item.group, common_item), :confirm => 'Are you sure?',:method => :delete
+      content_tag(:span, :class => "label important") do
+        link_to "Delete", group_common_item_path(common_item.group, common_item), :confirm => 'Are you sure?',:method => :delete
+      end
     end
   end
 end
